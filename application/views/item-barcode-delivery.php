245a8fe57a417d3f3e955f3d9f1347d8fba06010
brainwithE <haylyzh@gmail.com>
2016-08-10 16:45:48 +0800

    fix: delivery barcode qty

--- application/views/item-barcode-delivery.php
+++ application/views/item-barcode-delivery.php
@@ -5,16 +5,17 @@
 
 	foreach($item_list->result_array() as $row){ 
 		$delivery_id = $row['delivery_id'];
+		$item_qty = $row['delivery_quantity'];
 		$item_code = $row['item_id'];		
 		$item_price = $row['item_price'];
 		
 		
-		for($x = 0; $x <= 24; $x++){
+		for($x = 0; $x <= $item_qty; $x++){
 		?>
 			<div class="col-md-4 barcodes" id="bcTarget<?php echo $item_code.$x; ?>"></div>
 			<script>
 				$(document).ready(function(){	
-					for (i = 0; i < 24; i++) { 
+					for (i = 0; i < <?php echo $item_qty;?>; i++) { 
 				    $("#bcTarget"+<?php echo $item_code?>+i).barcode('<?php echo $item_code; ?>', "code39");
 				    $("#bcTarget"+<?php echo $item_code?>+i).append('<span style="margin-left: 15px;"><?php echo $supp; ?></span>');
 				    $("#bcTarget"+<?php echo $item_code?>+i).append('<span style="float:right;margin-right:15px;">Php <?php echo $item_price; ?></span>');
@@ -46,4 +47,3 @@ $(document).ready(function(){
     
 });
 </script>
-	
